f9803250d1b9bbf049f33618e25e65ceb3a0f35d
Daijiro Wachi <daijiro.wachi@gmail.com>
2020-05-26 22:02:28 +0900

    feat: npm run build

---
+++ build/precache-manifest.dd2cbbc0ae30529b8558ad63eb1126ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8580f3942eab1ddc362d479f36913ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f157a6fd06e4e5800a",
+    "url": "/static/js/2.d7837c0c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d7837c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3b8d82ae3cb4c1c277",
+    "url": "/static/js/main.e20bbb78.chunk.js"
+  },
+  {
+    "revision": "619d52d336d6207e072c",
+    "url": "/static/js/runtime-main.35320c9d.js"
+  },
+  {
+    "revision": "5c1e671c6329f6fdd45402c5dc7a06e3",
+    "url": "/static/media/boxing.5c1e671c.png"
+  },
+  {
+    "revision": "bd15695241b693cb777ec36085305373",
+    "url": "/static/media/darts.bd156952.png"
+  },
+  {
+    "revision": "afc52e6bc0447e187c020eaa0666d757",
+    "url": "/static/media/suikyu.afc52e6b.png"
+  },
+  {
+    "revision": "fcb911aa1dcdf21cf183ebe9ff3d24d0",
+    "url": "/static/media/sumo.fcb911aa.png"
+  }
+]);
